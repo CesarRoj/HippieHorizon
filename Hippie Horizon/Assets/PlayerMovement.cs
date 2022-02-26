7f0336cae39dd03bc04c6c97f0c68d40f688bad9
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-02-26 11:49:29 -0800

    newPlat

---
+++ Hippie Horizon/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput < 0){
+            Flip();
+        }
+        else if(facingRight == true && moveInput > 0 ){
+            Flip();
+        }
+    }
+
+    void Flip(){
+        
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *=-1;
+        transform.localScale = Scaler;
+    }
+}
